c8d419b57bc7f708868a5729eaa58d5b56e76212
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-06-04 22:36:13 -0400

    home component styled, directs to MarketAnalysis component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     template: `
-        <body>
-            <div class="container" >
-                <h1>
-                    Hey there
-                </h1>
+        <div class="container fill-height" style="padding-top: 100px;">
+            <div class="row">
+                <div class="col-lg-1"></div>
+                    <div class="col-lg-10 text-center">
+                        <h1>
+                            A Sensible Approach to Financial
+                        </h1>
+                        <h1 class="big_font_120">SUCCESS</h1>
+                        <button
+                            type="button"
+                            class="btn btn-primary"
+                            (click)="onStartNow()">
+                            Start Now
+                        </button>
+                    </div>
             </div>
-        </body>
+        </div>
     `,
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-    constructor() { }
+    constructor(
+        private elementRef: ElementRef,
+        private router: Router
+    ) { }
 
     ngOnInit() {
-        ;
+        this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'pink';
+    }
+
+    // Route to Market Analysis
+    public onStartNow() {
+        console.log('redirect to Market Analysis tab');
+        this.router.navigate(['market-analysis']);
     }
 }
