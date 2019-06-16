12f85056f02e156c8e8e5dd7a3b80bc73e4024d0
Daniel Canas <danielcanas10@gmail.com>
2019-06-16 15:46:40 -0400

    HTTP Client and Mongoose Imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -24,7 +25,8 @@ import { SecurityAnalysisComponent } from './security-analysis/security-analysis
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ],
     providers: [],
     bootstrap: [AppComponent]
