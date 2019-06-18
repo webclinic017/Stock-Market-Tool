324b2c4a6ad683f9c36567f449a82fa9de15c60a
Daniel Canas <danielcanas10@gmail.com>
2019-06-18 16:45:07 -0400

    Added database and login/register user endpoints

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try
+    {
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, 'this_serves_as_a_long_string');
+        next();
+    } 
+    catch (error)
+    {
+        res.status(401).json({
+            message: "Authentication failed!"
+        });
+    }
+};
