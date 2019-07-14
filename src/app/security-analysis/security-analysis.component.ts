bc6c033ce214d0fac045f2a5d361063320edea79
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-07-14 01:31:19 -0400

    Styled summary

--- src/app/security-analysis/security-analysis.component.ts
+++ src/app/security-analysis/security-analysis.component.ts
@@ -29,39 +29,73 @@ export class OwnerSummary {
         <div class="container">
             <div class="security-body">
                 <div class="interact-bar">
-                    <h1>buttons and search bar</h1>
-                    <input #tickerInput type="text" placeholder="Enter Ticker symbol"/>
-                    <button class="btn-primary" (click)="handleTickerSearch(tickerInput)">Click</button>
+                    <div>
+                        <input #tickerInput type="text" placeholder="Enter Ticker symbol"/>
+                        <button class="btn-primary" (click)="handleTickerSearch(tickerInput)">Search</button>
+                    </div>
+                    <div>
+                        <button class="btn-primary">ML Model</button>
+                        <button class="btn-primary">Reported</button>
+                        <button class="btn-primary">Per Share</button>
+                        <button class="btn-primary">Per Dollar</button>
+                    </div>
                 </div>
                 <div class="owner-summary">
                     <h2 class="text-center">Owner's Summary</h2>
                     <div class="summary-columns">
                         <div class="summary-col">
-                            <span>Intrinsic Value</span>
-                            <span>1 Year Growth</span>
-                            <span>5 Year Growth</span>
+                            <div>
+                                <span style="padding-right: 3.2rem;">Intrinsic Value</span>
+                                <span>{{ this.finEnum[this.testData.acuteFin] }}</span>
+                            </div>
+                            <div>
+                                <span style="padding-right: 3rem;">1 Year Growth</span>
+                                <span>{{ this.testData.oneYearGrowth }}</span>
+                            </div>
+                            <div>
+                                <span style="padding-right: 3rem;">5 Year Growth</span>
+                                <span>{{ this.testData.fiveYearGrowth }}</span>
+                            </div>
                         </div>
                         <div class="summary-col">
-                            <span>Historic Financials</span>
-                            <span>Acute Financials</span>
-                            <span>Projected Financials</span>
+                            <div>
+                                <span style="padding-right: 4.3rem;">Historic Financials</span>
+                                <span>{{ this.finEnum[this.testData.historicFin] }}</span>
+                            </div>
+                            <div>
+                                <span style="padding-right: 5.4rem;">Acute Financials</span>
+                                <span>{{ this.finEnum[this.testData.acuteFin] }}</span>
+                            </div>
+                            <div>
+                                <span style="padding-right: 3rem;">Projected Financials</span>
+                                <span>{{ this.finEnum[this.testData.projectedFin] }}</span>
+                            </div>
                         </div>
                         <div class="summary-col">
-                            <span>Est. Time to Value:</span>
-                            <span>Sustainable Growth Rate</span>
-                            <span>Effect from Intrinsic Value</span>
+                            <div>
+                                <span style="padding-right: 7rem;">Est. Time to Value</span>
+                                <span>{{ this.testData.fiveYearGrowth }}</span>
+                            </div>
+                            <div>
+                                <span style="padding-right: 2.6rem;">Sustainable Growth Rate</span>
+                                <span>{{ this.testData.fiveYearGrowth }}</span>
+                            </div>
+                            <div>
+                                <span style="padding-right: 2.3rem;">Effect from Intrinsic Value</span>
+                                <span>{{ this.testData.fiveYearGrowth }}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="security-details">
                     <div class="cashflow">
-                        <h3>Discounted Cashflow</h3>
+                        <h3 class="cashflow-header">Discounted Cashflow</h3>
                     </div>
                     <div class="capital">
-                        <h3>Captial Structure</h3>
+                        <h3 class="cashflow-header">Captial Structure</h3>
                     </div>
                     <div class="management">
-                        <h3>Management Effectiveness</h3>
+                        <h3 class="cashflow-header">Management Effectiveness</h3>
                     </div>
                 </div>
             </div>
@@ -70,6 +104,18 @@ export class OwnerSummary {
     styleUrls: ['security-analysis.component.css']
 })
 export class SecurityAnalysisComponent implements OnInit {
+    public testData: OwnerSummary = {
+         intrinsicValue: 1212,
+         oneYearGrowth: 12121,
+         fiveYearGrowth: 1212,
+         historicFin: 0,
+         acuteFin: 0,
+         projectedFin: 1,
+         timeToValue: 1,
+         growthRate: 12,
+         effectOfIntrVal: 1211
+    }
+    public finEnum = FinancialsDisplayEnum;
 
     constructor() { }
 
@@ -77,5 +123,6 @@ export class SecurityAnalysisComponent implements OnInit {
 
     public handleTickerSearch(tickerInput: HTMLInputElement) {
         console.log(tickerInput.value);
+        console.log(this.finEnum[this.testData.acuteFin]);
     }
 }
