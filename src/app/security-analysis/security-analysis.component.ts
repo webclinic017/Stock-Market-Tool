dc9dc67759a9c1fc095a7ce7df7f2a90514e21d6
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-07-13 21:54:19 -0400

    Small styling fix.

--- src/app/security-analysis/security-analysis.component.ts
+++ src/app/security-analysis/security-analysis.component.ts
@@ -1,4 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+export enum FinancialsDisplayEnum {
+    'Healthy',
+    'Unhealthy'
+}
+
+// TODO: Get real values from Jimmy.
+export enum FinacialsEnum {
+    Healthy,
+    Unhealthy
+}
+
+// TODO: Replicate on backend.
+export class OwnerSummary {
+    public intrinsicValue: number;
+    public oneYearGrowth: number;
+    public fiveYearGrowth: number;
+    public historicFin: FinacialsEnum;
+    public acuteFin: FinacialsEnum;
+    public projectedFin: FinacialsEnum;
+    public timeToValue: number;
+    public growthRate: number;
+    public effectOfIntrVal: number;
+}
 
 @Component({
     selector: 'app-security-analysis',
@@ -7,6 +30,8 @@ import { Component, OnInit } from '@angular/core';
             <div class="security-body">
                 <div class="interact-bar">
                     <h1>buttons and search bar</h1>
+                    <input #tickerInput type="text" placeholder="Enter Ticker symbol"/>
+                    <button class="btn-primary" (click)="handleTickerSearch(tickerInput)">Click</button>
                 </div>
                 <div class="owner-summary">
                     <h2 class="text-center">Owner's Summary</h2>
@@ -42,10 +67,15 @@ import { Component, OnInit } from '@angular/core';
             </div>
         </div>
     `,
+    styleUrls: ['security-analysis.component.css']
 })
 export class SecurityAnalysisComponent implements OnInit {
 
     constructor() { }
 
     ngOnInit() { }
+
+    public handleTickerSearch(tickerInput: HTMLInputElement) {
+        console.log(tickerInput.value);
+    }
 }
