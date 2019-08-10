cbc138e6a3d97ec8caf973c1b1ff4c257f3ec4b6
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-08-10 12:22:09 -0400

    Small backend change to properly grap query param.

--- backend/routes/securitys.js
+++ backend/routes/securitys.js
@@ -4,12 +4,12 @@ const Security = require("../models/security");
 const router = express.Router();
 
 router.get("/info", (req, res, next) => {
-    Security.findOne({ ticker: req.body.ticker})
+    Security.findOne({ ticker: req.query.ticker})
     .then(security => {
         if(!security)
         {
             return res.status(401).json({
-                message: "Security not found in database!"
+                message: 'Security not found in database!'
             });
         }
         res.status(200).json({
