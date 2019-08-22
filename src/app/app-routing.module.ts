73f2ea3651ca6f8a2f23f41c3c396bbc37fa2ef5
matthewmsaucedo <themattsaucedo@gmail.com>
2019-08-21 20:53:42 -0400

    small format change.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-
 import { LoginComponent } from './authorization/login.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
