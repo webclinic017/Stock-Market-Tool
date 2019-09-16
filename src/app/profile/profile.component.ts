303f72f357a6e5fddf93c35290d8ddc3d41aea1b
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-09-15 21:53:16 -0400

    profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,18 +1,55 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../authorization/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-profile',
     template: `
-        <div class="container">
-            <h1>Hello, world!</h1>
+        <div class="container profile-body">
+            <div class="profile-row">
+                <div class="profile-component">
+                    <div class="portfolio">
+                        <h1>Hi</h1>
+                    </div>
+                    <div *ngIf="tickerIsLoaded" class="data">
+                    </div>
+                </div>
+                <div class="profile-component">
+                    <div class="portfolio">
+                        <h1>Hi</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="profile-row">
+                <div>
+                    <button
+                        (click)="handleLogoutClick()"
+                        class="btn btn-default">Logout</button>
+                </div>
+                <div *ngIf="tickerIsLoaded" class="profile-component">
+                    <div class="">
+                    </div>
+                </div>
+            </div>
         </div>
     `,
     styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-    constructor() { }
+    public tickerIsLoaded = false;
+
+    constructor(
+        public router: Router,
+        public authService: AuthService,
+        private _toastrService: ToastrService
+    ) { }
 
     ngOnInit(): void { }
 
+    public handleLogoutClick(): void {
+        this.authService.isLoggedIn.next(false);
+        this._toastrService.success('Logout successful');
+        this.router.navigate(['market-analysis']);
+    }
 }
