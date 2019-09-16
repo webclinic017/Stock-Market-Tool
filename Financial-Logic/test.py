aef77549fd64b5bbcc51ffae7d671befc062ea74
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-09-16 16:59:55 -0400

    Delete extraneous financial folders

--- Financial-Logic/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-#The return on equity ratio measures how efficiently a company is using its equity to generate profit:
-    def roe(netIncome, shareholdersEquity):
-        print("ROE: " + netIncome / shareholdersEquity)
-
-roe(sys.argv[1], sys.argv[2])
